846964535abdb14fdd24a949953100cfa2bef925
Andrew <pold.jacobo89@gmail.com>
2024-10-03 16:14:57 -0500

    Se implemento eliminacion de detalle de materiales para cotizar

--- scripts/despliegue-orden/despliegueOrdenInterna.js
+++ scripts/despliegue-orden/despliegueOrdenInterna.js
@@ -176,7 +176,7 @@ $(document).ready(() => {
                     </td>
                     <td>
                         <div class="d-flex justify-content-around">
-                            <button class="btn btn-sm btn-danger btn-cotizacion-material">
+                            <button class="btn btn-sm btn-danger btn-delete-detalle-material">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
                                     <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"/>
                                 </svg>
@@ -190,6 +190,11 @@ $(document).ready(() => {
         // abrimos el modal
         const dialogCotizacion = new bootstrap.Modal(document.getElementById('cotizacionesModal'))
         dialogCotizacion.show()
+    })
+
+    $('#tbl-cotizaciones-materiales tbody').on('click', '.btn-delete-detalle-material', (event) => {
+        const $element = $(event.currentTarget).closest('tr')
+        $element.remove()
     })
 
     $('#proveedoresInput').on('input', debounce(async function () {
