f4f6ef3b04ec8d5ef76619f2db326f1fb87ade79
Andrew <pold.jacobo89@gmail.com>
2025-03-04 20:04:00 -0500

    Se agrego variable de entorno de conig

--- scripts/config.js
+++ scripts/config.js
@@ -2,5 +2,6 @@ const config = {
     XAMPP_CARPET: 'logistica',
     BACK_URL: 'http://localhost:8080/api',
     BACK_STORAGE_URL: 'http://localhost:8080/storage/',
-    FRONT_URL: 'http://localhost/logistica'
+    FRONT_URL: 'http://localhost/logistica',
+    BACK_EXTRANET_URL: 'http://logistica.famaisealjet.com:8083/api'
 }
