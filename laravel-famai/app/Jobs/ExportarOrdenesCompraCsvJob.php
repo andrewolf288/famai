1ee0b8303205da74560002027955e67e3c97c3cf
Andrew <pold.jacobo89@gmail.com>
2025-04-10 16:03:53 -0500

    Modify job export ordenes de compra

--- laravel-famai/app/Jobs/ExportarOrdenesCompraCsvJob.php
+++ laravel-famai/app/Jobs/ExportarOrdenesCompraCsvJob.php
@@ -35,7 +35,7 @@ class ExportarOrdenesCompraCsvJob implements ShouldQueue
     public function handle()
     {
         // Ruta donde se guardaran los archivos
-        $rutaDestino = "C:\\OrdenesCompra\\";
+        $rutaDestino = "/home/andrewdev/export/";
         // Verifica si la carpeta existe, si no la crea
         if (!file_exists($rutaDestino)) {
             mkdir($rutaDestino, 0777, true);
@@ -58,138 +58,134 @@ class ExportarOrdenesCompraCsvJob implements ShouldQueue
             ->get();
 
         // creamos la cabecera del csv
-            $csvCabecera = Writer::createFromPath($pathcsvCabeceraTemp, 'w');
+        $csvCabecera = Writer::createFromPath($pathcsvCabeceraTemp, 'w');
+        // $csvCabecera->setEnclosure(' ');
+        $csvCabecera->insertOne([
+            'DocNum',
+            'DocEntry',
+            'DocType',
+            'DocDate',
+            'DocDueDate',
+            'CardCode',
+            'DocTotal',
+            'DocCurrency',
+            'DocRate',
+            'Comments',
+            'PaymentGroupCode',
+            'U_EXX_CORDOCOR',
+            // 'TaxDate',
+            // 'DocTotalFc',
+            // 'VatPercent',
+        ]);
+        $csvCabecera->insertOne([
+            'DocNum',
+            'DocEntry',
+            'DocType',
+            'DocDate',
+            'DocDueDate',
+            'CardCode',
+            'DocTotal',
+            'DocCur',
+            'DocRate',
+            'Comments',
+            'GroupNum',
+            'U_EXX_CORDOCOR',
+            // 'TaxDate',
+            // 'DocTotalFC',
+            // 'VatPercent',
+        ]);
+
+        // creamos el detalle del csv
+        $csvDetalle = Writer::createFromPath($pathcsvDetalleTemp, 'w');
+        // $csvDetalle->setEnclosure(' ');
+        $csvDetalle->insertOne([
+            "ParentKey",
+            "LineNum",
+            "ItemCode",
+            "Quantity",
+            "ShipDate",
+            "Price",
+            "Currency",
+            "DiscountPercent",
+            // "TaxCode",
+            "LineTotal",
+            "TaxPercentagePerRow",
+            "U_FAM_FECINOC",
+            "ItemDetails"
+            // "GrossTotal"
+            // "TaxTotal"
+        ]);
+        $csvDetalle->insertOne([
+            "DocNum",
+            "LineNum",
+            "ItemCode",
+            "Quantity",
+            "ShipDate",
+            "Price",
+            "Currency",
+            "DiscPrcnt",
+            // "TaxCode",
+            "LineTotal",
+            "VatPrcnt",
+            "U_FAM_FECINOC",
+            "Text"
+            // "GTotal"
+            // "VatSum"
+        ]);
+
+        $contador = 1;
+        // generamos el csv de cabecera
+        foreach ($ordenescompra as $key => $orden) {
             $csvCabecera->insertOne([
-                'DocNum',
-                'DocEntry',
-                'DocType',
-                'DocDate',
-                'DocDueDate',
-                'CardCode',
-                'Address',
-                'DocTotal',
-                'DocCurrency',
-                'DocRate',
-                'Comments',
-                'PaymentGroupCode',
-                'U_EXX_CORDOCOR',
-                'U_EXX_FECDOCOT'
-                // 'TaxDate',
-                // 'DocTotalFc',
-                // 'VatPercent',
-            ]);
-            $csvCabecera->insertOne([
-                'DocNum',
-                'DocEntry',
-                'DocType',
-                'DocDate',
-                'DocDueDate',
-                'CardCode',
-                'Address',
-                'DocTotal',
-                'DocCur',
-                'DocRate',
-                'Comments',
-                'GroupNum',
-                'U_EXX_CORDOCOR',
-                'U_EXX_FECDOCOT'
-                // 'TaxDate',
-                // 'DocTotalFC',
-                // 'VatPercent',
+                $contador, // DocNum
+                $contador, // DocEntry
+                'I', // DocType (standard for purchase order)
+                UtilHelper::formatDateExportSAP($orden->occ_fecha), // DocDate
+                UtilHelper::formatDateExportSAP($orden->occ_fecha), // DocDueDate
+                $orden->proveedor->prv_codigo, // CardCode
+                $orden->occ_total, // DocTotal (Total)
+                $orden->mon_codigo, // DocCur (Moneda)
+                $orden->occ_tipocambio ?? 1, // DocRate
+                UtilHelper::cleanForCSV($orden->occ_notas), // Comments (Comentarios)
+                $orden->fpa_codigo, // GroupNum (Forma de pago)
+                // UtilHelper::formatDateExportSAP($orden->occ_fecha), // TaxDate (assuming the same as DocDate)
+                // 0, // DocTotalFC (Total en moneda extranjera)
+                // 0, // VatPercent 
+                $orden->occ_numero,
             ]);
 
-            // creamos el detalle del csv
-            $csvDetalle = Writer::createFromPath($pathcsvDetalleTemp, 'w');
-            $csvDetalle->insertOne([
-                "ParentKey",
-                "LineNum",
-                "ItemCode",
-                "ItemDescription",
-                "Quantity",
-                "ShipDate",
-                "Price",
-                "Currency",
-                "DiscountPercent",
-                // "TaxCode",
-                "LineTotal",
-                "TaxPercentagePerRow",
-                "U_FAM_FECINOC"
-                // "GrossTotal"
-                // "TaxTotal"
-            ]);
-            $csvDetalle->insertOne([
-                "DocNum",
-                "LineNum",
-                "ItemCode",
-                "Dscription",
-                "Quantity",
-                "ShipDate",
-                "Price",
-                "Currency",
-                "DiscPrcnt",
-                // "TaxCode",
-                "LineTotal",
-                "VatPrcnt",
-                "U_FAM_FECINOC"
-                // "GTotal"
-                // "VatSum"
-            ]);
+            $ordenescompradetalle = OrdenCompraDetalle::with('producto')
+                ->where('occ_id', $orden->occ_id)
+                ->get();
 
-            $contador = 1;
-            // generamos el csv de cabecera
-            foreach ($ordenescompra as $key => $orden) {
-                $csvCabecera->insertOne([
+            $contadorDetalle = 0;
+
+            foreach ($ordenescompradetalle as $detalle) {
+                $csvDetalle->insertOne([
                     $contador, // DocNum
-                    $contador, // DocEntry
-                    'I', // DocType (standard for purchase order)
-                    UtilHelper::formatDateExportSAP($orden->occ_fecha), // DocDate
-                    UtilHelper::formatDateExportSAP($orden->occ_fecha), // DocDueDate
-                    $orden->proveedor->prv_codigo, // CardCode
-                    UtilHelper::cleanForCSV($orden->proveedor->prv_direccion), // Address 
-                    $orden->occ_total, // DocTotal (Total)
-                    $orden->mon_codigo, // DocCur (Moneda)
-                    $orden->occ_tipocambio ?? 1, // DocRate
-                    UtilHelper::cleanForCSV($orden->occ_notas), // Comments (Comentarios)
-                    $orden->fpa_codigo, // GroupNum (Forma de pago)
-                    // UtilHelper::formatDateExportSAP($orden->occ_fecha), // TaxDate (assuming the same as DocDate)
-                    // 0, // DocTotalFC (Total en moneda extranjera)
-                    // 0, // VatPercent 
-                    $orden->occ_numero,
-                    UtilHelper::formatDateExportSAP($orden->occ_feccreacion)
+                    $contadorDetalle, // LineNum
+                    $detalle->producto->pro_codigo, // ItemCode
+                    $detalle->ocd_cantidad, // Quantity
+                    UtilHelper::formatDateExportSAP($detalle->ocd_fechaentrega), // ShipDate
+                    $detalle->ocd_preciounitario, // Price
+                    $orden->mon_codigo, // Currency
+                    $detalle->ocd_porcentajedescuento,
+                    // $detalle->imp_codigo, // TaxCode
+                    $detalle->ocd_total, // LineTotal
+                    $detalle->ocd_porcentajeimpuesto, // VatPrcnt
+                    // $detalle->ocd_total, // GrossTotal
+                    // '', // VatSum (por calcular o llenar según lógica)
+                    UtilHelper::formatDateExportSAP($detalle->ocd_fechaentrega), // U_FAM_FECINOC (campo obligatorio)
+                    UtilHelper::cleanForCSV($detalle->ocd_observacion)
                 ]);
 
-                $ordenescompradetalle = OrdenCompraDetalle::with('producto')
-                    ->where('occ_id', $orden->occ_id)
-                    ->get();
-
-                $contadorDetalle = 0;
-
-                foreach ($ordenescompradetalle as $detalle) {
-                    $csvDetalle->insertOne([
-                        $contador, // DocNum
-                        $contadorDetalle, // LineNum
-                        $detalle->producto->pro_codigo, // ItemCode
-                        UtilHelper::cleanForCSV($detalle->ocd_descripcion), // Dscription
-                        $detalle->ocd_cantidad, // Quantity
-                        UtilHelper::formatDateExportSAP($detalle->ocd_fechaentrega), // ShipDate
-                        $detalle->ocd_preciounitario, // Price
-                        $orden->mon_codigo, // Currency
-                        $detalle->ocd_porcentajedescuento,
-                        // $detalle->imp_codigo, // TaxCode
-                        $detalle->ocd_total, // LineTotal
-                        $detalle->ocd_porcentajeimpuesto, // VatPrcnt
-                        // $detalle->ocd_total, // GrossTotal
-                        // '', // VatSum (por calcular o llenar según lógica)
-                        UtilHelper::formatDateExportSAP($detalle->ocd_fechaentrega), // U_FAM_FECINOC (campo obligatorio)
-                    ]);
-
-                    $contadorDetalle++;
-                }
-
-                $contador++;
+                $contadorDetalle++;
             }
 
-            // al finalizar la importacion, cambiamos el flag de importacion
-            OrdenCompra::whereIn('occ_id', $ordenescompra->pluck('occ_id'))->update(['occ_importacion' => 1]);
+            $contador++;
+        }
+
+        // al finalizar la importacion, cambiamos el flag de importacion
+        OrdenCompra::whereIn('occ_id', $ordenescompra->pluck('occ_id'))->update(['occ_importacion' => 1]);
     }
 }
