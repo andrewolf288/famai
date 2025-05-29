9d3df56eb543528cd0f3fd6884943292f1d0cc6d
Dani <vilcadaniel53@gmail.com>
2025-05-29 16:35:01 -0500

    fix

--- scripts/despliegue-orden-resumido/despliegueOrdenResumido.js
+++ scripts/despliegue-orden-resumido/despliegueOrdenResumido.js
@@ -914,7 +914,7 @@ $(document).ready(async () => {
                                 <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"/>
                             </svg>
                         </button>
-                        <button class="btn btn-primary asignar-codigo" data-odm-id="${detalle.odm_id}" style="width: 130px;">Validar Código</button>
+                        <button class="btn btn-primary asignar-codigo" data-odm-id="${detalle.odm_id}" disabled style="width: 155px;">Reasignar Código</button>
                     </div>
                 </td>
             </tr>`
