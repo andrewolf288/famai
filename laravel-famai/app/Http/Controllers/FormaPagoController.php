52011504ba60591a295d609a20a2436ef052780b
Dani <vilcadaniel53@gmail.com>
2025-07-10 10:15:39 -0500

    sincronizacion automatica con formas de pago de sap

--- laravel-famai/app/Http/Controllers/FormaPagoController.php
+++ laravel-famai/app/Http/Controllers/FormaPagoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\FormaPago;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class FormaPagoController extends Controller
 {
@@ -15,6 +16,11 @@ class FormaPagoController extends Controller
 
     public function indexSimple()
     {
+        try {
+            DB::statement('EXEC dbo.SincronizarFormasPago');
+        } catch (\Throwable $th) {
+            // 
+        }
         $formasPago = FormaPago::where('fpa_activo', 1)->select('fpa_codigo', 'fpa_descripcion')->get();
         return response()->json($formasPago);
     }
