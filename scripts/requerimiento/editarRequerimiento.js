3aa2b4067f712eeb0f7c4df64c7fddf46d9f3192
Dani <vilcadaniel53@gmail.com>
2025-09-05 14:35:15 -0500

    fix: productos que no tienen codigo

--- scripts/requerimiento/editarRequerimiento.js
+++ scripts/requerimiento/editarRequerimiento.js
@@ -100,7 +100,7 @@ $(document).ready(async () => {
         parte.materiales.forEach((material) => {
           const row = `
                         <tr>
-                            <td >${material["producto"]["pro_codigo"]}</td>
+                            <td >${material["producto"]?.["pro_codigo"] ?? "N/A"}</td>
 
                             <td>
                                 <input type="text" class="form-control descripcion-input" value='${material["odm_descripcion"].replace(
@@ -113,7 +113,7 @@ $(document).ready(async () => {
                                 <input type="number" class="form-control cantidad-input" value='${material["odm_cantidad"]}' readonly/>
                             </td>
 
-                            <td>${material["producto"]["uni_codigo"] ?? ""}</td>
+                            <td>${material["producto"]?.["uni_codigo"] ?? "N/A"}</td>
 
                             <td>
                                 <input type="text" class="form-control observacion-input" value='${
@@ -124,21 +124,21 @@ $(document).ready(async () => {
                             <td>
                                 <div class="d-flex justify-content-around">
                                     <button class="btn btn-sm btn-warning btn-detalle-producto-editar me-2" data-producto="${
-                                      material["producto"]["pro_id"]
+                                      material["producto"]?.["pro_id"]
                                     }" data-odm-id="${material["odm_id"]}">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
                                             <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
                                         </svg>
                                     </button>
                                     <button class="btn btn-sm btn-danger btn-detalle-producto-eliminar me-2" data-producto="${
-                                      material["producto"]["pro_id"]
+                                      material["producto"]?.["pro_id"]
                                     }" data-odm-id="${material["odm_id"]}">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
                                             <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
                                         </svg>
                                     </button>
                                     <button class="btn btn-sm btn-primary btn-detalle-producto-adjuntos" data-producto="${
-                                      material["producto"]["pro_id"]
+                                      material["producto"]?.["pro_id"]
                                     }" data-odm-id="${material["odm_id"]}">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-text-fill" viewBox="0 0 16 16">
                                             <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0M9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1M4.5 9a.5.5 0 0 1 0-1h7a.5.5 0 0 1 0 1zM4 10.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m.5 2.5a.5.5 0 0 1 0-1h4a.5.5 0 0 1 0 1z"/>
