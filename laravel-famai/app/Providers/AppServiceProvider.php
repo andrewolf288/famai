06bdd4046b4faf58c75f86a99573e8adcf39efed
Dani <vilcadaniel53@gmail.com>
2025-11-25 10:24:10 -0500

    setear el lenguaje antes de cada sql

--- laravel-famai/app/Providers/AppServiceProvider.php
+++ laravel-famai/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Se ejecuta cada vez que Laravel abre una conexión
+        try {
+            DB::connection()->getPdo()->exec("SET LANGUAGE us_english");
+            DB::connection()->getPdo()->exec("SET DATEFORMAT ymd");
+        } catch (\Exception $e) {
+            // La conexión aún no está establecida, se ignorará el error
+            // Los comandos se ejecutarán cuando se use la conexión por primera vez
+        }
     }
 }
